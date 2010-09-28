2642c5e58b14b33fe684370ba5fec58d5a67c5f5
Joshua Falcon <Joshua@Joshua-Falcons-MacBook-Pro.local>
2010-09-28 19:24:32 -0400

    New classes for proxy preferences that I did not include in my other commit.

---
+++ platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/ui/http/preferencepage/HTTPPreferenceInitializer.java
@@ -0,0 +1,23 @@
+package com.subgraph.vega.ui.http.preferencepage;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import com.subgraph.vega.internal.ui.http.Activator;
+
+public class HTTPPreferenceInitializer extends AbstractPreferenceInitializer {
+
+	public HTTPPreferenceInitializer() {
+		// TODO Auto-generated constructor stub
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault("ProxyPort",8888);
+		
+	}
+
+	@Override
+	public void initializeDefaultPreferences() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
