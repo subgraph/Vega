5d64face15252460c37ae9d11e4e4d25fa57b391
Hugo Fortier <hugo@subgraph.com>
2010-10-08 06:40:07 -0400

    SSL support for Http Request

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/HttpClientFactory.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/HttpClientFactory.java
@@ -1,5 +1,15 @@
 package com.subgraph.vega.internal.http.requests;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
 import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.params.ClientPNames;
@@ -39,6 +49,35 @@ public class HttpClientFactory {
 	private static SchemeRegistry createSchemeRegistry() {
 		final SchemeRegistry sr = new SchemeRegistry();
 		sr.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+
+	
+		SSLContext ctx;
+		try {
+			ctx = SSLContext.getInstance("TLS");
+			X509TrustManager tm = new X509TrustManager() {
+
+			       public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+			       }
+
+			       public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+			       }
+
+			       public X509Certificate[] getAcceptedIssuers() {
+			           return null;
+			        }
+			 };
+			 ctx.init(null, new X509TrustManager[]{tm}, null);
+			 SSLSocketFactory ssf = new SSLSocketFactory(ctx);
+			 ssf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			 sr.register(new Scheme("https", ssf, 443));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return sr;
 	}
 }
