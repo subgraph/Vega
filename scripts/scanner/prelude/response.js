0e595e4bc6bae69c629e29746446ec49f746bc47
brl <bruce@netifera.com>
2010-10-19 02:29:08 -0400

    Cleaned up and added convenient access to important HttpResponse methods.

--- scripts/scanner/prelude/response.js
+++ scripts/scanner/prelude/response.js
@@ -4,45 +4,63 @@ var httpResponse = null;
 var wrappedResponse = null;
 
 __defineGetter__("response", function() {
-    if(!httpResponse)
+     if(!httpResponse) {
+    	 print("no http response");
       return null;
+	}
     return (wrappedResponse || (wrappedResponse = new Response(httpResponse)));
 });
 
 function Response(httpResponse) {
-  this.httpResponse = httpResponse.rawResponse;
+  this.httpResponse = httpResponse;
+  this.rawResponse = httpResponse.rawResponse;
   this.entityString = null;
+  this.cachedDocument = null;
 }
 
 Response.prototype.header = function(name) {
-  var hdr = this.httpResponse.getFirstHeader(name);
+  var hdr = this.rawResponse.getFirstHeader(name);
   return (hdr && hdr.value);
 };
 
 Response.prototype.consumeContent = function() {
-  if(this.httpResponse.entity)
-    this.httpResponse.entity.consumeContent();
+  if(this.rawResponse.entity)
+    this.rawResponse.entity.consumeContent();
 };
 
 Response.prototype.__defineGetter__("bodyAsString", function() {
-    var e = this.httpResponse.entity;
+    var e = this.rawResponse.entity;
     if(!e) {
-      print("e is null :(");
       return null; /* or "" ? */
     }
     if(this.entityString) {
-      print("returning cached :"+ this.entityString);
       return this.entityString;
     }
     this.entityString = EntityUtils.toString(e);
-    print("returning calculated: "+ this.entityString);
     return this.entityString;
     /* XXX exceptions */
     //return (this.entityString || (this.entityString = EntityUtils.toString(e)));
 });
 
+Response.prototype.__defineGetter__("allHeaders", function() {
+	return this.rawResponse.allHeaders;
+});
+
+Response.prototype.headers = function(name) {
+	return this.rawResponse.getHeaders(name);
+};
+
+Response.prototype.__defineGetter__("document", function()  {
+	if(!this.httpResponse.getDocumentDOM()) {		
+		return null;
+	}
+	if(!this.cachedDocument)
+		this.cachedDocument = new Document(this.httpResponse.getDocumentDOM());
+	return this.cachedDocument;
+});
+
 Response.prototype.__defineGetter__("code", function() {
-    return this.httpResponse.statusLine.statusCode;
+    return this.rawResponse.statusLine.statusCode;
 });
 
 
