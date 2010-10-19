4be7aac8da512141eac7669572961218cfd97851
brl <bruce@netifera.com>
2010-10-19 02:30:12 -0400

    Fixed exception when pattern does not match.

--- scripts/scanner/modules/response/vinfo-emails.js
+++ scripts/scanner/modules/response/vinfo-emails.js
@@ -17,7 +17,7 @@ function run() {
     var match;
     var regexp2 = /([-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?)/;
     match = regexp2.exec(res[0]); 
-    if (emails.indexOf(match[0]) < 0)
+    if ((match != null) && emails.indexOf(match[0]) < 0)
     {
       emails.push(match[0]);
     }
