c32501252469379c0919c89a7208c963a600b002
brl <bruce@netifera.com>
2010-10-20 19:26:50 -0400

    Tweak application window settings.

--- platform/com.subgraph.vega.application/src/com/subgraph/vega/application/ApplicationWorkbenchWindowAdvisor.java
+++ platform/com.subgraph.vega.application/src/com/subgraph/vega/application/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,8 @@
 package com.subgraph.vega.application;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -18,11 +20,26 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     
     public void preWindowOpen() {
         IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(1000, 600));
+        configurer.setInitialSize(new Point(1200, 900));
         configurer.setShowCoolBar(true);
+        configurer.setShowPerspectiveBar(true);
         configurer.setShowStatusLine(true);
         configurer.setShowMenuBar(true);
-        configurer.setShowPerspectiveBar(true);
         configurer.setShowFastViewBars(true);
+        PlatformUI.getPreferenceStore().setValue(
+				IWorkbenchPreferenceConstants.DOCK_PERSPECTIVE_BAR, IWorkbenchPreferenceConstants.TOP_RIGHT);
+		PlatformUI.getPreferenceStore().setValue(
+				IWorkbenchPreferenceConstants.SHOW_MEMORY_MONITOR, true);
+		PlatformUI.getPreferenceStore().setValue(
+				IWorkbenchPreferenceConstants.PERSPECTIVE_BAR_EXTRAS, "com.subgraph.vega.perspectives.scanner, com.subgraph.vega.perspectives.proxy");
+
+		PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.SHOW_OPEN_ON_PERSPECTIVE_BAR, false);
+		// For some reason this seems to do nothing
+		PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.PERSPECTIVE_BAR_SIZE, 100);
+
+
     }
+
+
+    
 }
