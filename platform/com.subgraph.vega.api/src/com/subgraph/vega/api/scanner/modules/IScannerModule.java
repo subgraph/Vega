3c8271dd268122c957056b7e30ff527a556ea4b9
brl <bruce@netifera.com>
2011-03-08 18:54:56 -0500

    OCD

--- platform/com.subgraph.vega.api/src/com/subgraph/vega/api/scanner/modules/IScannerModule.java
+++ platform/com.subgraph.vega.api/src/com/subgraph/vega/api/scanner/modules/IScannerModule.java
@@ -1,7 +1,6 @@
 package com.subgraph.vega.api.scanner.modules;
 
 public interface IScannerModule {
-
 	String getModuleName();
 	ModuleScriptType getModuleType();
 	IScannerModuleRunningTime getRunningTimeProfile();
