75ff924e034af2c6ad1ac3b9273859b265dedde6
brl <bruce@netifera.com>
2011-03-25 00:15:33 -0400

    Old packages.  Not used anymore.

--- platform/com.subgraph.vega.api/src/com/subgraph/vega/api/requestlog/IRequestLogChangeEvent.java
+++
@@ -1,8 +0,0 @@
-package com.subgraph.vega.api.requestlog;
-
-import com.subgraph.vega.api.events.IEvent;
-
-public interface IRequestLogChangeEvent extends IEvent {
-	boolean isRecordAddEvent();
-	IRequestLogRecord getRecord();
-}
