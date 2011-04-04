929fa90cefc6f5a41f457977e8b3478795ecadca
brl <bruce@netifera.com>
2011-04-03 22:56:20 -0400

    Copy entity for entity enclosing requests.

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/VegaHttpClient.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/VegaHttpClient.java
@@ -5,16 +5,22 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpEntityEnclosingRequest;
 import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.params.DefaultedHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.DefaultedHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.util.EntityUtils;
 
 public class VegaHttpClient extends DefaultHttpClient {
 	private final Logger logger = Logger.getLogger("request-engine");
@@ -27,9 +33,10 @@ public class VegaHttpClient extends DefaultHttpClient {
 	}
 	
 	HttpRequest createProcessedRequest(HttpRequest originalRequest, HttpContext originalContext) {
-		final HttpRequest newRequest = new BasicHttpRequest(originalRequest.getRequestLine());
+		final HttpRequest newRequest = createNewRequest(originalRequest);
 		final HttpContext ctx = new BasicHttpContext(originalContext);
 		final HttpContext execCtx = new DefaultedHttpContext(ctx, defaultContext);
+		newRequest.setParams(new DefaultedHttpParams(newRequest.getParams(), getParams()));
 		for(Header h: originalRequest.getAllHeaders()) 
 			newRequest.addHeader(h.getName(), h.getValue());
 			
@@ -41,5 +48,34 @@ public class VegaHttpClient extends DefaultHttpClient {
 			logger.log(Level.WARNING, "Unexpected IOException building processed request", e);
 		}
 		return newRequest;
+	}
+	
+	private HttpRequest createNewRequest(HttpRequest originalRequest) {
+		if(originalRequest instanceof HttpEntityEnclosingRequest)
+			return createNewEntityRequest((HttpEntityEnclosingRequest) originalRequest);
+		else
+			return new BasicHttpRequest(originalRequest.getRequestLine());
+	}
+	
+	private HttpRequest createNewEntityRequest(HttpEntityEnclosingRequest originalRequest) {
+		final HttpEntityEnclosingRequest req = new BasicHttpEntityEnclosingRequest(originalRequest.getRequestLine());
+		final HttpEntity originalEntity = originalRequest.getEntity();
+		if(originalEntity != null)
+			req.setEntity(copyEntity(originalEntity));
+		return req;		
+	}
+	
+	private HttpEntity copyEntity(HttpEntity entity) {
+		if(entity == null)
+			return null;
+		try {
+			final ByteArrayEntity newEntity = new ByteArrayEntity(EntityUtils.toByteArray(entity));
+			newEntity.setContentEncoding(entity.getContentEncoding());
+			newEntity.setContentType(entity.getContentType());
+			return newEntity;
+		} catch (IOException e) {
+			logger.log(Level.WARNING, "I/O error reading entity", e);
+			return null;
+		}
 	}
 }
