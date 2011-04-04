575e50860f9994108a1497beffd79ae2d0d991f1
brl <bruce@netifera.com>
2011-04-03 23:00:43 -0400

    Set HTTP version and user agent string.

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/HttpClientFactory.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/HttpClientFactory.java
@@ -24,8 +24,15 @@ import com.subgraph.vega.internal.http.requests.unencoding.UnencodingThreadSafeC
 
 public class HttpClientFactory {
 
+	private static final String userAgent = 
+		"User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; "+
+          "Trident/4.0; .NET CLR 1.1.4322; InfoPath.1; .NET CLR "+
+          "2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; Vega/1.0";
+          
 	static VegaHttpClient createHttpClient() {
 		final HttpParams params = createHttpParams();
+		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setUserAgent(params, userAgent);
 		final ClientConnectionManager ccm = createConnectionManager(params);
 		final VegaHttpClient client = new VegaHttpClient(ccm, params);
 		client.getParams().setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, false);
