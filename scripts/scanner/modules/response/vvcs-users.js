05ed286520c8424953ea4947ba3966d80e10e7a3
dma <dma@subgraph.com>
2011-05-07 22:02:21 -0400

    Updated alerts with new content, changed alert templates to have multiple remediation, small changes to modules.

--- scripts/scanner/modules/response/vvcs-users.js
+++ scripts/scanner/modules/response/vvcs-users.js
@@ -1,16 +1,11 @@
 var module = {
-  name : "VCS Module",
+  name : "Version Control String Detection",
   type: "response-processor"
 };
 
 function run() {
-  var banner = response.header("Server");
-  var host = response.header("Host");
   var regexp = /\$.{1,12}: .*? .*? \d{4}[-\/]\d{1,2}[-\/]\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.* (.*?) (Exp )?\$/;
   var res;
-
-  if(!banner)
-	  return;
 
   res = regexp.test(response.bodyAsString);
   if (res) {
