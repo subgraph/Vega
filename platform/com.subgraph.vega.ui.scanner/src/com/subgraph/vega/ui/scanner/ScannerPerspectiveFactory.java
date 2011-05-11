9eac461ab04e29fdd79c4bf0c4a5cf9df5f028c4
brl <bruce@subgraph.com>
2011-05-10 23:00:35 -0400

    Default placeholder added to perspective so that request editors appear in the right place.

--- platform/com.subgraph.vega.ui.scanner/src/com/subgraph/vega/ui/scanner/ScannerPerspectiveFactory.java
+++ platform/com.subgraph.vega.ui.scanner/src/com/subgraph/vega/ui/scanner/ScannerPerspectiveFactory.java
@@ -1,5 +1,6 @@
 package com.subgraph.vega.ui.scanner;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -9,12 +10,13 @@ import com.subgraph.vega.ui.scanner.info.ScanInfoView;
 public class ScannerPerspectiveFactory implements IPerspectiveFactory {
 	private final static String WEBSITE_VIEW = "com.subgraph.vega.views.website";
 
-
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		layout.addView(ScanInfoView.ID, IPageLayout.TOP, 0f, layout.getEditorArea());
+		final IFolderLayout folder = layout.createFolder("main", IPageLayout.TOP, 0, layout.getEditorArea());
+		folder.addView(ScanInfoView.ID);
+		folder.addPlaceholder("*:*");
 		layout.addView(WEBSITE_VIEW, IPageLayout.LEFT, 0.25f, ScanInfoView.ID);
-		layout.addView(ScanAlertView.ID, IPageLayout.BOTTOM, 0.80f, ScanInfoView.ID);
+		layout.addView(ScanAlertView.ID, IPageLayout.BOTTOM, 0.80f, "main");
 		layout.setEditorAreaVisible(false);
 	}
 
