6927ea7f36cb2d1cca7eee4aaf2eb85edc5bd04b
brl <bruce@subgraph.com>
2011-05-17 18:37:00 -0400

    Inital UI changes for configuration of response processing modules in proxy

---
+++ platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/ui/http/commands/ConfigureProxyModules.java
@@ -0,0 +1,16 @@
+package com.subgraph.vega.ui.http.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ConfigureProxyModules extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("clicky click");
+		return null;
+	}
+
+
+}
