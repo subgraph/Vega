14dedb6f78387d97fd8001197fb2e526e57b6287
brl <bruce@subgraph.com>
2011-05-22 23:23:22 -0400

    a) clean up deprecated API usage from HC upgrade b) allow configuring connection limits though request engine config

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/HttpRequestEngineFactory.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/HttpRequestEngineFactory.java
@@ -4,6 +4,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 
 import com.subgraph.vega.api.html.IHTMLParser;
 import com.subgraph.vega.api.http.requests.IHttpRequestEngine;
@@ -11,7 +13,7 @@ import com.subgraph.vega.api.http.requests.IHttpRequestEngineConfig;
 import com.subgraph.vega.api.http.requests.IHttpRequestEngineFactory;
 
 public class HttpRequestEngineFactory implements IHttpRequestEngineFactory {
-	private final static int NTHREADS = 12;
+	private final static int NTHREADS = 25;
 	private final ExecutorService executor = Executors.newFixedThreadPool(NTHREADS);
 	private final HttpClient client = UnencodedHttpClientFactory.createHttpClient();
 	private IHTMLParser htmlParser;
@@ -28,14 +30,25 @@ public class HttpRequestEngineFactory implements IHttpRequestEngineFactory {
 	
 	@Override
 	public IHttpRequestEngine createRequestEngine(IHttpRequestEngineConfig config) {
+		configureClient(client, config);
 		return new HttpRequestEngine(executor, client, config, htmlParser);
 	}
 
 	@Override
 	public IHttpRequestEngine createRequestEngine(HttpClient client, IHttpRequestEngineConfig config) {
+		configureClient(client, config);
 		return new HttpRequestEngine(executor, client, config, htmlParser);
 	}
 	
+	private void configureClient(HttpClient client, IHttpRequestEngineConfig config) {
+		final ClientConnectionManager connectionManager = client.getConnectionManager();
+		if(connectionManager instanceof ThreadSafeClientConnManager) {
+			ThreadSafeClientConnManager ccm = (ThreadSafeClientConnManager) connectionManager;
+			ccm.setMaxTotal(config.getMaxConnections());
+			ccm.setDefaultMaxPerRoute(config.getMaxConnectionsPerRoute());
+		}
+	}
+
 	protected void setHTMLParser(IHTMLParser htmlParser) {
 		this.htmlParser = htmlParser;
 	}
@@ -43,5 +56,4 @@ public class HttpRequestEngineFactory implements IHttpRequestEngineFactory {
 	protected void unsetHTMLParser(IHTMLParser htmlParser) {
 		this.htmlParser = null;
 	}
-
 }
