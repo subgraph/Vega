6416afb5a94f329be3c7b2db39837e265389256a
Cade Cairns <cairnsc@subgraph.com>
2011-06-21 18:33:55 -0400

    Remove redundant User-Agent part of string

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/UnencodedHttpClientFactory.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/UnencodedHttpClientFactory.java
@@ -24,7 +24,7 @@ import com.subgraph.vega.internal.http.requests.connection.UnencodingThreadSafeC
 
 public class UnencodedHttpClientFactory extends AbstractHttpClientFactory {
 	private static final String userAgent = 
-		"User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; "+
+		"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; "+
           "Trident/4.0; .NET CLR 1.1.4322; InfoPath.1; .NET CLR "+
           "2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; Vega/1.0";
           
