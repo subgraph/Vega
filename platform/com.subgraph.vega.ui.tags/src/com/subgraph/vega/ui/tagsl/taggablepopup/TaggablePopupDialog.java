0b45754e2bcb2c8e8fd5d59f82d467e1d16d8447
Cade Cairns <cairnsc@subgraph.com>
2011-12-14 11:35:50 -0500

    Clean up taggable popup dialog. Now displays colors.

--- platform/com.subgraph.vega.ui.tags/src/com/subgraph/vega/ui/tagsl/taggablepopup/TaggablePopupDialog.java
+++ platform/com.subgraph.vega.ui.tags/src/com/subgraph/vega/ui/tagsl/taggablepopup/TaggablePopupDialog.java
@@ -10,10 +10,18 @@
  ******************************************************************************/
 package com.subgraph.vega.ui.tagsl.taggablepopup;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.eclipse.jface.dialogs.PopupDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -28,11 +36,13 @@ public class TaggablePopupDialog extends PopupDialog {
 	private Composite parentComposite;
 	private ITaggable taggable;
 	private Point origin;
+	private List<Label> tagLabelList;
 	
 	public TaggablePopupDialog(Shell parent, ITaggable taggable, Point origin) {
 		super(parent, HOVER_SHELLSTYLE, false, false, false, false, false, null, null);
 		this.taggable = taggable;
 		this.origin = origin;
+		
 	}
 
 	@Override
@@ -40,8 +50,22 @@ public class TaggablePopupDialog extends PopupDialog {
 		parentComposite = new Composite(parent, SWT.NONE);
 		parentComposite.setLayout(new GridLayout(1, false));
 
-		for (ITag tag: taggable.getAllTags()) {
-			createTagLabel(parentComposite, tag);
+		Label header = new Label(parentComposite, SWT.NONE);
+		header.setText("Tags:");
+		FontData[] fd = header.getFont().getFontData();
+		fd[0].setStyle(SWT.BOLD);
+		final Font newFont = new Font(parentComposite.getDisplay(), fd[0]);
+		header.setFont(newFont);
+		header.addDisposeListener(new DisposeListener() {
+		    public void widgetDisposed(DisposeEvent e) {
+		    	newFont.dispose();
+		    }
+		});
+
+		final Collection<ITag> tagList = taggable.getAllTags();
+		tagLabelList = new ArrayList<Label>(tagList.size());
+		for (ITag tag: tagList) {
+			tagLabelList.add(createTagLabel(parentComposite, tag));
 		}
 
 		return parentComposite;
@@ -61,11 +85,33 @@ public class TaggablePopupDialog extends PopupDialog {
 	protected Point getInitialLocation(Point initialSize) {
 		return origin;
 	}
+	
+	@Override
+	protected Color getBackground() {
+		return parentComposite.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
+	}
 
-	private void createTagLabel(Composite parent, ITag tag) {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	protected List getForegroundColorExclusions() {
+		final List superExclusions = super.getForegroundColorExclusions();
+		final List exclusions = new ArrayList(superExclusions.size() + tagLabelList.size());
+		exclusions.addAll(superExclusions);
+		exclusions.addAll(tagLabelList);
+		return exclusions;
+	}
+	
+	private Label createTagLabel(Composite parent, ITag tag) {
 		final Label label = new Label(parent, SWT.NONE);
 		label.setText(tag.getName());
-		label.setForeground(tagColorToColor(label.getDisplay(), tag.getNameColor()));
+		final Color color = tagColorToColor(label.getDisplay(), tag.getNameColor());
+		label.setForeground(color);
+		label.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				color.dispose();
+			}
+		});
+		return label;
 	}
 
 	private Color tagColorToColor(Device device, int color) {
