3bb8a68686e4c9fe4166001bcdab77ff83a778b0
dma <dma@subgraph.com>
2012-09-23 16:43:53 -0400

    File upload detection module. Stored XSS XML.
    TBD.
    
    Signed-off-by: dma <dma@subgraph.com>

---
+++ scripts/scanner/modules/response/vfileupload.js
@@ -0,0 +1,34 @@
+var module = {
+  name: "File Upload Detection",
+  type: "response-processor"
+};
+
+function run(request, response, ctx) {
+  var found = 0;
+
+  if (response.document) {
+    var input = jQuery("input:file", response.document);
+    input.each(function() {
+    	ctx.debug(this.innerHTML);
+    	found++;
+    });
+  }
+
+  if (found) {
+    var match;
+    var sub = request.requestLine.uri;
+    var index = sub.indexOf('?');
+
+    if (index >= 0) {
+      sub = sub.substring(0, index);
+    }
+
+    ctx.addRegexHighlight("type=file");
+    (found > 1) ? match = "instances" : match = "instance";
+    ctx.alert("vautocomplete", request, response, {
+      "resource": request.requestLine.uri,
+      key: "vautocomplete:" + sub
+    });
+
+  }
+}
