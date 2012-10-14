6cdbb91a9d779430b386fe49eaa257f06117d90f
brl <bruce@subgraph.com>
2012-10-13 20:51:18 -0400

    removed unused imports

--- platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/ui/http/commands/StopProxyHandler.java
+++ platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/ui/http/commands/StopProxyHandler.java
@@ -11,7 +11,6 @@
 package com.subgraph.vega.ui.http.commands;
 
 import com.subgraph.vega.api.http.proxy.IHttpProxyService;
-import com.subgraph.vega.ui.http.Activator;
 
 public class StopProxyHandler extends AbstractProxyCommandHandler {
 	@Override
