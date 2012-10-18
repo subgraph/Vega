e8238202dd9a1a9c700a4bb87c00620ace92259a
brl <bruce@subgraph.com>
2012-10-17 21:42:37 -0400

    Use new http client TrustStrategy to override certificate verification

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/AbstractHttpClientFactory.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/AbstractHttpClientFactory.java
@@ -10,51 +10,32 @@
  ******************************************************************************/
 package com.subgraph.vega.internal.http.requests;
 
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
 
 public abstract class AbstractHttpClientFactory {
 
 	protected static SchemeRegistry createSchemeRegistry() {
 		final SchemeRegistry sr = new SchemeRegistry();
 		sr.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
-
-		SSLContext ctx;
 		try {
-			ctx = SSLContext.getInstance("TLS");
-			X509TrustManager tm = new X509TrustManager() {
-				public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+			SSLSocketFactory ssf = new SSLSocketFactory(new TrustStrategy() {
+				@Override
+				public boolean isTrusted(X509Certificate[] chain, String authType)
+						throws CertificateException {
+					return true;
 				}
-
-				public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-				}
-
-				public X509Certificate[] getAcceptedIssuers() {
-					return null;
-				}
-			};
-			ctx.init(null, new X509TrustManager[]{tm}, null);
-			SSLSocketFactory ssf = new SSLSocketFactory(ctx, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			}, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 			sr.register(new Scheme("https", 443, ssf));
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (KeyManagementException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (Exception e) {
+			throw new RuntimeException("Unexpected exception creating SSLSocketFactory", e);
 		}
-
 		return sr;
 	}
-
 }
