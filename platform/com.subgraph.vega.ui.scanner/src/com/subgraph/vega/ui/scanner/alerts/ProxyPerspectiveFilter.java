fcae2ca17a295e6d96cbeea0cbe64a04d3cf4348
brl <bruce@subgraph.com>
2012-10-24 05:23:46 -0400

    handle IScanAlert items in filter

--- platform/com.subgraph.vega.ui.scanner/src/com/subgraph/vega/ui/scanner/alerts/ProxyPerspectiveFilter.java
+++ platform/com.subgraph.vega.ui.scanner/src/com/subgraph/vega/ui/scanner/alerts/ProxyPerspectiveFilter.java
@@ -3,14 +3,18 @@ package com.subgraph.vega.ui.scanner.alerts;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
+import com.subgraph.vega.api.model.alerts.IScanAlert;
 import com.subgraph.vega.api.model.alerts.IScanAlertRepository;
 
 public class ProxyPerspectiveFilter extends ViewerFilter {
 	@Override
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
 		if(element instanceof IAlertTreeNode) {
-			IAlertTreeNode node = (IAlertTreeNode) element;
+			final IAlertTreeNode node = (IAlertTreeNode) element;
 			return node.getScanInstance().getScanId() == IScanAlertRepository.PROXY_ALERT_ORIGIN_SCAN_ID;
+		} else if(element instanceof IScanAlert) {
+			final IScanAlert alert = (IScanAlert) element;
+			return alert.getScanId() == IScanAlertRepository.PROXY_ALERT_ORIGIN_SCAN_ID;
 		}
 		return false;
 	}
