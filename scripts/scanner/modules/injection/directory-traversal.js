395a82795e553f2cdfe95db6fccc94f0a74a6809
dma <dma@subgraph.com>
2012-12-23 14:32:57 -0500

    Module enhancements.
    Module enhancements.

--- scripts/scanner/modules/injection/directory-traversal.js
+++ scripts/scanner/modules/injection/directory-traversal.js
@@ -10,17 +10,47 @@ function initialize(ctx) {
 }
 
 function createInjectables(ctx) {
-  var ps = ctx.pathState;
+  var ps = ctx.getPathState();
+
   if (!ps.isParametric()) {
-    return ["/./", "/.vega/", "\\.\\", "\\.vega\\"];
+    return ["/./", "/.vega/", 
+            "\\.\\", "\\.vega\\"];
   }
   var fuzzable = ps.getFuzzableParameter();
-  var injectables = [".../", "./", "...\\", ".\\"];
+
+  var injectables = [".../", "./", 
+                     "...\\", ".\\", 
+                     "/../", "/.../", 
+                     "\\..\\", "\\...\\",
+                     "/./", "/././", "/.../",
+                     "/../../../../../../../../../../../etc/passwd\0",
+                     "../../../../../../../../../../../etc/passwd\0"];
+  
+
   var ret = [];
+ 
   for (var i = 0; i < injectables.length; i++)
-  ret.push(injectables[i] + fuzzable.value);
+    ret.push(injectables[i] + fuzzable.value);
+  
+  // UNIX 
+  
+  ret.push("/../../../../../../../../../../../etc/passwd");
+  ret.push("../../../../../../../../../../../../etc/passwd");
+  ret.push("file:/../../../../../../../../../../../../etc/passwd");
+  
+  // WINDOWS
+  
+ // ret.push("\..\..\..\")
+  // JAVA
+  
+  ret.push("WEB-INF/web.xml");
+  ret.push("/WEB-INF/web.xml");
+  ret.push("../../WEB-INF/web.xml");
+  ret.push("/../../WEB-INF/web.xml");
+  
   return ret;
 }
+
 
 function handler(req, res, ctx) {
   if (ctx.hasModuleFailed()) return;
@@ -29,39 +59,93 @@ function handler(req, res, ctx) {
     ctx.setModuleFailed();
     return;
   }
-
-  ctx.addRequestResponse(req, res);
-  if (ctx.incrementResponseCount() < 4) return;
-
+  
   var ps = ctx.getPathState();
-  var fp = ps.getPathFingerprint();
+  var fp = ps.getPathFingerprint();	
+  
+  ctx.addRequestResponse(req, res);
+  
+  if (!ps.isParametric()) {
+	  if (ctx.incrementResponseCount() < 4) return;
+  }
+  else {
+	  if (ctx.incrementResponseCount() < 20) return;
+  }
+  
 
   if (!ps.isParametric()) {
     if (ctx.getSavedResponse(0).code < 300 && !ctx.isFingerprintMatch(0, fp) && !ctx.isFingerprintMatch(0, 1)) {
-      publishAlert(ctx, "Unique response for /./", 0, req, res);
+      publishAlert(ctx, "Unique response for /./", 0, ctx.getSavedRequest(0), ctx.getSavedResponse(0));
       ctx.responseChecks(ps.createRequest(), ctx.getSavedResponse(0));
     }
     if (ctx.getSavedResponse(2).code < 300 && !ctx.isFingerprintMatch(2, fp) && !ctx.isFingerprintMatch(2, 3)) {
-      publishAlert(ctx, "Unique response for \\.\\", 2, req, res);
-      ctx.responseChecks(2);
-    }
-  } else {
-    if (!ctx.isFingerprintMatch(0, 1)) {
-      publishAlert(ctx, "Responses for ./val and .../val look different", 1, req, res);
-      ctx.responseChecks(0);
-    }
-    if (!ctx.isFingerprintMatch(2, 3)) {
-      publishAlert(ctx, "Responses for .\\val and ...\\val look different", 3, req, res);
+      publishAlert(ctx, "Unique response for \\.\\", 2, ctx.getSavedRequest(2), ctx.getSavedResponse(2));
       ctx.responseChecks(2);
     }
   }
+  else {
+    if (!ctx.isFingerprintMatch(0, 1)) {
+      publishAlert(ctx, "Responses for ./val and .../val look different", 1);
+      ctx.responseChecks(0);
+    }
+    if (!ctx.isFingerprintMatch(2, 3)) {
+      publishAlert(ctx, "Responses for .\\val and ...\\val look different", 3);
+      ctx.responseChecks(2);
+    }
+    if (!ctx.isFingerprintMatch(4, 5)) {
+        publishAlert(ctx, "Responses for /../val and /.../val look different", 5);
+        ctx.responseChecks(4);
+      }
+    if (!ctx.isFingerprintMatch(6, 7)) {
+        publishAlert(ctx, "Responses for \..\val and \...\val look different", 7);
+        ctx.responseChecks(6);
+      }
+    if (ctx.isFingerprintMatch(8, 9) && (!ctx.isFingerprintMatch(8,10))) {
+    	publishAlert(ctx, "Responses for /./, /././ and /.../ look different", 10);
+    	ctx.responseChecks(8);
+    }
+  }  
+    // Passwd file content matches
+    
+    var pwre = /root:.:0:0/;
+    var i;
+    for (i=11; i<=15; i++) {
+    	res = pwre.test(ctx.getSavedResponse(i).bodyAsString);
+    	if (res) {
+    		publishAlert(ctx, "", i);
+    	}
+    }
+
+    // web.xml content matches
+
+    var content = "<web-app";
+    for (i = 16; i<= 19; i++) {
+    	if (ctx.getSavedResponse(i).bodyAsString.indexOf(content) >= 0) {
+    		publishAlert(ctx, "", i);
+    	}
+    }
+    
+    
 }
 
+  
+  
 function publishAlert(ctx, msg, idx) {
 
-  ctx.alert("vinfo-directory-traversal", ctx.getSavedRequest(idx), ctx.getSavedResponse(idx), {
-    output: ctx.getSavedResponse(idx).bodyAsString,
-    key: "vinfo-1918:" + ctx.getSavedRequest(idx).requestLine.uri,
-    resource: ctx.getSavedRequest(idx).requestLine.uri
-  });
+	var uri = String(ctx.getSavedRequest(idx).requestLine.uri);
+	var uripart = uri.replace(/\?.*/, "");
+	var ps = ctx.getPathState();
+	
+	ctx.alert("vinfo-directory-traversal", ctx.getSavedRequest(idx), ctx.getSavedResponse(idx), {
+		output: ctx.getSavedResponse(idx).bodyAsString,
+		key: "vinfo-directory-traversal:" + uripart + ps.getFuzzableParameter().name,
+		resource: uripart
+	});
 }
+
+	
+	
+
+
+
+
