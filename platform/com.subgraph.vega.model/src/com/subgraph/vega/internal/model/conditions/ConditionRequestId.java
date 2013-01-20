f8d9466bcf35d01d9560aef74e5a62cc4de69109
brl <bruce@subgraph.com>
2013-01-20 15:39:29 -0500

    A new condition type for matching an exact request id

---
+++ platform/com.subgraph.vega.model/src/com/subgraph/vega/internal/model/conditions/ConditionRequestId.java
@@ -0,0 +1,74 @@
+package com.subgraph.vega.internal.model.conditions;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+
+import com.db4o.query.Constraint;
+import com.db4o.query.Query;
+import com.subgraph.vega.api.model.conditions.IHttpCondition;
+import com.subgraph.vega.api.model.conditions.IHttpConditionType;
+import com.subgraph.vega.api.model.conditions.match.IHttpConditionMatchAction;
+import com.subgraph.vega.api.model.requests.IRequestLogRecord;
+import com.subgraph.vega.internal.model.conditions.match.IntegerMatchActionSet;
+
+public class ConditionRequestId extends AbstractCondition {
+
+	private transient static IHttpConditionType conditionType;
+	
+	private static IHttpConditionType getConditionType() {
+		synchronized (ConditionRequestId.class) {
+			if(conditionType == null) {
+				conditionType = createType();
+			}
+			return conditionType;
+		}
+	}
+	
+	private static IHttpConditionType createType() {
+		return new ConditionType("request id", new IntegerMatchActionSet(), true) {
+			@Override
+			public IHttpCondition createConditionInstance(
+					IHttpConditionMatchAction matchAction) {
+				return new ConditionRequestId(matchAction);
+			}
+		};
+	}
+	private ConditionRequestId(IHttpConditionMatchAction matchAction) {
+		super(matchAction);
+	}
+
+	@Override
+	public IHttpConditionType getType() {
+		return getConditionType();
+	}
+	
+	@Override
+	public boolean matches(IRequestLogRecord record) {
+		return matchesInteger((int) record.getRequestId());
+	}
+	
+	@Override
+	public boolean matches(HttpRequest request) {
+		return false;
+	}
+
+	@Override
+	public boolean matches(HttpResponse response) {
+		return false;
+	}
+
+	@Override
+	public boolean matches(HttpRequest request, HttpResponse response) {
+		return false;
+	}
+
+	@Override
+	public boolean isInternal() {
+		return true;
+	}
+
+	@Override
+	public Constraint filterRequestLogQuery(Query query) {
+		return constrainQuery(query.descend("requestId"));
+	}
+}
