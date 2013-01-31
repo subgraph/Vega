bb66c7a659ebc9e100bebdc28bddcad0fa002661
brl <bruce@subgraph.com>
2013-01-31 15:40:16 -0500

    remove unused import

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/CookieMatcher.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/CookieMatcher.java
@@ -1,7 +1,6 @@
 package com.subgraph.vega.internal.http.requests;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
