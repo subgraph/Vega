1286d2fc8f2778df72057d6d8e741df0d7f29c9d
brl <bruce@subgraph.com>
2013-02-05 09:59:22 -0500

    Synchronize scanner debug preferences with proxy scan configuration when proxy scan enabled/disabled

--- platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/ui/http/commands/ProxyScanHandler.java
+++ platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/ui/http/commands/ProxyScanHandler.java
@@ -3,12 +3,18 @@ package com.subgraph.vega.ui.http.commands;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.subgraph.vega.api.http.proxy.IHttpProxyService;
+import com.subgraph.vega.api.scanner.IScannerConfig;
 import com.subgraph.vega.ui.http.Activator;
 
 public class ProxyScanHandler extends AbstractHandler {
+	public static final String SCANNER_PLUGIN = "com.subgraph.vega.ui.scanner";
+	public static final String P_LOG_ALL_REQUESTS = "LogAllRequests";
+	public static final String P_DISPLAY_DEBUG_OUTPUT = "DisplayDebugOutput";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -16,7 +22,19 @@ public class ProxyScanHandler extends AbstractHandler {
 		boolean oldValue = HandlerUtil.toggleCommandState(event.getCommand());
 		
 		final IHttpProxyService proxyService = Activator.getDefault().getProxyService();
+		IScannerConfig scannerConfig = proxyService.getProxyScanConfig();
+		if(scannerConfig != null) {
+			configureScanner(scannerConfig);
+		}
 		proxyService.setProxyScanEnabled(!oldValue);
 		return null;
+	}
+	
+	private void configureScanner(IScannerConfig config) {
+		final IPreferencesService preferencesService = Platform.getPreferencesService();
+		final boolean logAll = preferencesService.getBoolean(SCANNER_PLUGIN, P_LOG_ALL_REQUESTS, false, null);
+		final boolean displayDebug = preferencesService.getBoolean(SCANNER_PLUGIN, P_DISPLAY_DEBUG_OUTPUT, false, null);
+		config.setLogAllRequests(logAll);
+		config.setDisplayDebugOutput(displayDebug);
 	}
 }
