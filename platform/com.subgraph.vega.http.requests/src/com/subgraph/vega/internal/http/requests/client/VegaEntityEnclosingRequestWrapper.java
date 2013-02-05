17b61f18fa413e47222479fdf3a38b96a3dc405a
brl <bruce@subgraph.com>
2013-02-05 17:14:58 -0500

    Make our custom request wrappers 'abortable'

--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/client/VegaEntityEnclosingRequestWrapper.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/client/VegaEntityEnclosingRequestWrapper.java
@@ -1,12 +1,17 @@
 package com.subgraph.vega.internal.http.requests.client;
 
+import java.io.IOException;
+
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.ProtocolException;
 import org.apache.http.RequestLine;
+import org.apache.http.client.methods.AbortableHttpRequest;
+import org.apache.http.conn.ClientConnectionRequest;
+import org.apache.http.conn.ConnectionReleaseTrigger;
 import org.apache.http.impl.client.EntityEnclosingRequestWrapper;
 
-public class VegaEntityEnclosingRequestWrapper extends EntityEnclosingRequestWrapper {
-
+public class VegaEntityEnclosingRequestWrapper extends EntityEnclosingRequestWrapper implements AbortableHttpRequest {
+ 
 	public VegaEntityEnclosingRequestWrapper(HttpEntityEnclosingRequest request)
 			throws ProtocolException {
 		super(request);
@@ -15,5 +20,41 @@ public class VegaEntityEnclosingRequestWrapper extends EntityEnclosingRequestWra
 	@Override
 	public RequestLine getRequestLine() {
 		return getOriginal().getRequestLine();
+	}
+
+	private AbortableHttpRequest getAbortableRequest() {
+		if(getOriginal() instanceof AbortableHttpRequest) {
+			return (AbortableHttpRequest) getOriginal();
+		} else {
+			return null;
+		}
+	}
+	
+	@Override
+	public void setConnectionRequest(ClientConnectionRequest connRequest)
+			throws IOException {
+		final AbortableHttpRequest ab = getAbortableRequest();
+		if(ab != null) {
+			ab.setConnectionRequest(connRequest);
+		}
+	}
+
+	@Override
+	public void setReleaseTrigger(ConnectionReleaseTrigger releaseTrigger)
+			throws IOException {
+		final AbortableHttpRequest ab = getAbortableRequest();
+		if(ab != null) {
+			ab.setReleaseTrigger(releaseTrigger);
+		}
+	}
+	
+	@Override
+	public void abort() {
+		final AbortableHttpRequest ab = getAbortableRequest();
+		if(ab != null) {
+			ab.abort();
+		} else {
+			super.abort();
+		}
 	}
 }
--- platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/client/VegaRequestWrapper.java
+++ platform/com.subgraph.vega.http.requests/src/com/subgraph/vega/internal/http/requests/client/VegaRequestWrapper.java
@@ -1,11 +1,16 @@
 package com.subgraph.vega.internal.http.requests.client;
 
+import java.io.IOException;
+
 import org.apache.http.HttpRequest;
 import org.apache.http.ProtocolException;
 import org.apache.http.RequestLine;
+import org.apache.http.client.methods.AbortableHttpRequest;
+import org.apache.http.conn.ClientConnectionRequest;
+import org.apache.http.conn.ConnectionReleaseTrigger;
 import org.apache.http.impl.client.RequestWrapper;
 
-public class VegaRequestWrapper extends RequestWrapper {
+public class VegaRequestWrapper extends RequestWrapper implements AbortableHttpRequest {
 
 	public VegaRequestWrapper(HttpRequest request) throws ProtocolException {
 		super(request);
@@ -16,4 +21,40 @@ public class VegaRequestWrapper extends RequestWrapper {
 		return getOriginal().getRequestLine();
 	}
 
+	
+	private AbortableHttpRequest getAbortableRequest() {
+		if(getOriginal() instanceof AbortableHttpRequest) {
+			return (AbortableHttpRequest) getOriginal();
+		} else {
+			return null;
+		}
+	}
+	
+	@Override
+	public void setConnectionRequest(ClientConnectionRequest connRequest)
+			throws IOException {
+		final AbortableHttpRequest ab = getAbortableRequest();
+		if(ab != null) {
+			ab.setConnectionRequest(connRequest);
+		}
+	}
+
+	@Override
+	public void setReleaseTrigger(ConnectionReleaseTrigger releaseTrigger)
+			throws IOException {
+		final AbortableHttpRequest ab = getAbortableRequest();
+		if(ab != null) {
+			ab.setReleaseTrigger(releaseTrigger);
+		}
+	}
+	
+	@Override
+	public void abort() {
+		final AbortableHttpRequest ab = getAbortableRequest();
+		if(ab != null) {
+			ab.abort();
+		} else {
+			super.abort();
+		}
+	}
 }
