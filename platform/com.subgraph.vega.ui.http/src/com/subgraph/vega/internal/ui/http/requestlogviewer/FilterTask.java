0f3f240ff4c4ff9b53535862e6f27eb23d7a68b4
brl <bruce@subgraph.com>
2013-02-11 19:45:28 -0500

    New content provider implementation for request view table

---
+++ platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/internal/ui/http/requestlogviewer/FilterTask.java
@@ -0,0 +1,182 @@
+package com.subgraph.vega.internal.ui.http.requestlogviewer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Display;
+
+import com.subgraph.vega.api.model.conditions.IHttpConditionSet;
+import com.subgraph.vega.api.model.requests.IRequestLog;
+import com.subgraph.vega.api.model.requests.IRequestLogRecord;
+
+public class FilterTask {
+	private final static int UPDATE_VIEWER_INTERVAL = 500;
+	private final static int INITIAL_CHUNK_SIZE = 200;
+	private final static int LOAD_CHUNK_SIZE = 1000;
+	private final IRequestLog requestLog;
+	private final IHttpConditionSet filter;
+	private final TableViewer viewer;
+	private final Display display;
+	/* records with (id < nextRequestId) */
+	private final List<IRequestLogRecord> records;
+	/* records with (id >= nextRequestId) */
+	private final List<IRequestLogRecord> addedRecords;
+	private final Thread thread;
+	private final Object lock = new Object();
+	
+	private volatile boolean isStarted;
+	private volatile boolean isCancelled;
+	private boolean isFinished;
+	private volatile long nextRequestId;
+	private int lastSize;
+	
+	FilterTask(TableViewer viewer, IRequestLog requestLog, IHttpConditionSet filter) {
+		this.viewer = viewer;
+		this.display = viewer.getControl().getDisplay();
+		this.requestLog = requestLog;
+		this.filter = filter;
+		this.records = new ArrayList<IRequestLogRecord>();
+		this.addedRecords = new ArrayList<IRequestLogRecord>();
+		this.thread = new Thread(createRunnable());
+	}
+	
+	IRequestLogRecord getRecordForIndex(int index) {
+		synchronized(lock) {
+			if(index < records.size()) {
+				return records.get(index);
+			}
+		}
+		return null;
+	}
+	
+	void addNewRecord(IRequestLogRecord record) {
+		synchronized (lock) {
+			if(isFinished) {
+				records.add(record);
+			} else {
+				if(record.getRequestId() >= nextRequestId) {
+					addedRecords.add(record);
+				}
+			}
+		}
+	}
+	
+	void applyFilter() {
+		synchronized (lock) {
+			if(isStarted) {
+				return;
+			}
+			nextRequestId = requestLog.getNextRequestId();
+			isStarted = true;
+			thread.start();
+		}
+	}
+
+	void cancel() {
+		isCancelled = true;
+	}
+	
+	private void loadRecords() {
+		final Iterator<IRequestLogRecord> it = requestLog.getRecordIteratorByConditionSet(filter);
+		if(loadChunk(it, INITIAL_CHUNK_SIZE)) {
+			return;
+		}
+		if(records.size() > 0) {
+			selectViewerItem(records.get(0));
+		}
+		while(it.hasNext()) {
+			if(loadChunk(it, LOAD_CHUNK_SIZE)) {
+				return;
+			}
+		}
+
+		performFinalMerge();
+	}
+
+	private boolean loadChunk(Iterator<IRequestLogRecord> it, int chunkSize) {
+		final int size;
+		synchronized(lock) {
+			for(int i = 0; i < chunkSize && it.hasNext(); i++) {
+				if(isCancelled) {
+					return true;
+				}
+				final IRequestLogRecord record = it.next();
+				if(record.getRequestId() < nextRequestId) {
+					records.add(record);
+				}
+			}
+			size = records.size();
+		}
+		applyRecordCountToViewer(size);
+		return false;
+	}
+	
+	private void performFinalMerge() {
+		final int size;
+		synchronized (lock) {
+			records.addAll(addedRecords);
+			addedRecords.clear();
+			size = records.size();
+			isFinished = true;
+		}
+		applyRecordCountToViewer(size);
+	}
+
+	
+	
+	private void selectViewerItem(final IRequestLogRecord record) {
+		display.asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				if(!viewer.getControl().isDisposed() && !isCancelled) {
+					viewer.setSelection(new StructuredSelection(record));
+				}
+			}
+			
+		});
+	}
+	private void applyRecordCountToViewer(final int recordCount) {
+		display.asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				if(!viewer.getControl().isDisposed() && !isCancelled) {
+					viewer.setItemCount(recordCount);
+					viewer.refresh(false);
+				}
+			}
+		});
+	}
+	
+	private Runnable createRunnable() {
+		return new Runnable() {
+			@Override
+			public void run() {
+				loadRecords();
+				runUpdateViewerLoop();
+			}
+		};
+	}
+	
+	private void runUpdateViewerLoop() {
+		while(!isCancelled) {
+			try {
+				Thread.sleep(UPDATE_VIEWER_INTERVAL);
+			} catch (InterruptedException e) {
+				return;
+			}
+			if(isCancelled) {
+				return;
+			}
+			synchronized(lock) {
+				if(records.size() != lastSize) {
+					lastSize = records.size();
+					applyRecordCountToViewer(lastSize);
+				}
+			}
+		}
+	}
+}
---
+++ platform/com.subgraph.vega.ui.http/src/com/subgraph/vega/internal/ui/http/requestlogviewer/RequestViewContentProvider.java
@@ -0,0 +1,203 @@
+package com.subgraph.vega.internal.ui.http.requestlogviewer;
+
+import org.eclipse.jface.viewers.ILazyContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.graphics.Color;
+
+import com.subgraph.vega.api.events.IEvent;
+import com.subgraph.vega.api.events.IEventHandler;
+import com.subgraph.vega.api.model.IModel;
+import com.subgraph.vega.api.model.IWorkspace;
+import com.subgraph.vega.api.model.WorkspaceCloseEvent;
+import com.subgraph.vega.api.model.WorkspaceOpenEvent;
+import com.subgraph.vega.api.model.WorkspaceResetEvent;
+import com.subgraph.vega.api.model.conditions.ConditionSetChanged;
+import com.subgraph.vega.api.model.conditions.IHttpConditionManager;
+import com.subgraph.vega.api.model.conditions.IHttpConditionSet;
+import com.subgraph.vega.api.model.requests.IRequestLog;
+import com.subgraph.vega.api.model.requests.IRequestLogNewRecordListener;
+import com.subgraph.vega.api.model.requests.IRequestLogRecord;
+import com.subgraph.vega.api.model.requests.RequestLogNewRecordEvent;
+
+public class RequestViewContentProvider implements ILazyContentProvider, IRequestLogNewRecordListener {
+
+	private final String conditionSetId;
+	private final Color activeFilterColor;
+	private final IEventHandler conditionSetListener;
+	
+	private IModel model;
+	private IWorkspace currentWorkspace;
+	private IEventHandler workspaceListener;
+	private TableViewer tableViewer;
+	
+	private IHttpConditionSet filterCondition;
+	private FilterTask currentResult;
+	
+	public RequestViewContentProvider(String instanceId, Color activeFilterColor) {
+		if(instanceId != null) {
+			conditionSetId = IHttpConditionManager.CONDITION_SET_FILTER + "." + instanceId;
+		} else {
+			conditionSetId = IHttpConditionManager.CONDITION_SET_FILTER;
+		}
+		conditionSetListener = createConditionSetListener();
+		workspaceListener = createWorkspaceListener();
+		this.activeFilterColor = activeFilterColor;
+	}
+	
+	public IHttpConditionSet getConditionSet() {
+		return filterCondition;
+	}
+
+	@Override
+	public void dispose() {
+		cleanupListeners();
+		activeFilterColor.dispose();
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		if(!(viewer instanceof TableViewer)) {
+			tableViewer = null;
+			return;
+		}
+		tableViewer = (TableViewer) viewer;
+		
+		if(newInput instanceof IModel) {
+			setNewInput((IModel) newInput);
+		} else {
+			setNullInput();
+		}
+			
+	}
+
+	private void setNullInput() {
+		cleanupListeners();
+		tableViewer.setItemCount(0);
+		model = null;
+		currentWorkspace = null;
+		filterCondition = null;
+	}
+	
+	private void setNewInput(IModel model) {
+		this.model = model;
+		if(model == null) {
+			setNullInput();
+			return;
+		}
+		filterCondition = model.addConditionSetTracker(conditionSetId, conditionSetListener);
+		filterCondition.setMatchOnEmptySet(true);
+		
+		currentWorkspace = model.addWorkspaceListener(workspaceListener);
+		if(currentWorkspace != null) {
+			currentWorkspace.getRequestLog().addNewRecordListener(this, filterCondition);
+		}
+		reloadRecords();
+	}
+	
+	private void cleanupListeners() {
+		if(currentWorkspace != null) {
+			currentWorkspace.getRequestLog().removeNewRecordListener(this);
+		}
+		if(model != null) {
+			if(filterCondition != null) {
+				model.removeConditionSetTracker(conditionSetId, conditionSetListener);
+				filterCondition = null;
+			}
+			model.removeWorkspaceListener(workspaceListener);
+		}
+	}
+
+	@Override
+	public void updateElement(int index) {
+		if(currentResult == null) {
+			return;
+		}
+		final IRequestLogRecord record = currentResult.getRecordForIndex(index);
+		if(record != null) {
+			tableViewer.replace(record, index);
+		}
+	}
+
+	private IEventHandler createConditionSetListener() {
+		return new IEventHandler() {
+
+			@Override
+			public void handleEvent(IEvent event) {
+				if(event instanceof ConditionSetChanged) {
+					final IHttpConditionSet conditionSet = ((ConditionSetChanged) event).getConditionSet();
+					setConditionFilter(conditionSet);
+				}
+			}
+		};
+	}
+	
+	private IEventHandler createWorkspaceListener() {
+		return new IEventHandler() {
+			@Override
+			public void handleEvent(IEvent event) {
+				if(event instanceof WorkspaceOpenEvent)
+					onWorkspaceOpen((WorkspaceOpenEvent) event);
+				else if(event instanceof WorkspaceResetEvent)
+					onWorkspaceReset((WorkspaceResetEvent) event);
+				else if(event instanceof WorkspaceCloseEvent)
+					onWorkspaceClose((WorkspaceCloseEvent) event);
+				reloadRecords();
+			}			
+		};
+	}
+	private void onWorkspaceOpen(WorkspaceOpenEvent event) {
+		currentWorkspace.getRequestLog().removeNewRecordListener(this);
+		currentWorkspace = event.getWorkspace();
+		currentWorkspace.getRequestLog().addNewRecordListener(this, filterCondition);
+	}
+	
+	private void onWorkspaceReset(WorkspaceResetEvent event) {
+		currentWorkspace.getRequestLog().removeNewRecordListener(this);
+		currentWorkspace = event.getWorkspace();
+		currentWorkspace.getRequestLog().addNewRecordListener(this, filterCondition);
+		
+	}
+	
+	private void onWorkspaceClose(WorkspaceCloseEvent event) {
+		currentWorkspace.getRequestLog().removeNewRecordListener(this);
+	}
+
+	private void setConditionFilter(IHttpConditionSet conditionSet) {
+		if(conditionSet != null) {
+			conditionSet.setMatchOnEmptySet(true);
+		}
+		highlightTableForConditionFilter(conditionSet);
+		final IRequestLog requestLog = currentWorkspace.getRequestLog();
+		filterCondition = conditionSet;
+		requestLog.removeNewRecordListener(this);
+		requestLog.addNewRecordListener(this, conditionSet);
+		reloadRecords();
+	}
+
+	private void highlightTableForConditionFilter(IHttpConditionSet conditionSet) {
+		if(conditionSet != null && conditionSet.hasActiveConditions(false)) {
+			tableViewer.getTable().setBackground(activeFilterColor);
+		} else {
+			tableViewer.getTable().setBackground(null);
+		}
+	}
+
+	private void reloadRecords() {
+		if(currentResult != null) {
+			currentResult.cancel();
+		}
+		if(currentWorkspace == null) {
+			return;
+		}
+		currentResult = new FilterTask(tableViewer, currentWorkspace.getRequestLog(), filterCondition);
+		currentResult.applyFilter();
+	}
+
+	@Override
+	public void onNewRecord(RequestLogNewRecordEvent event) {
+		if(currentResult != null) {
+			currentResult.addNewRecord(event.getNewRecord());
+		}
+	}
+}
