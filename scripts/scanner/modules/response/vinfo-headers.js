355eaeac7aadfe528101127aac8898b8ae234a06
dasmodem <mckinney@subgraph.com>
2013-02-22 16:04:49 -0500

    Added modules and alerts for checking certain headers and missing charset.

---
+++ scripts/scanner/modules/response/vinfo-headers.js
@@ -0,0 +1,38 @@
+var module = {
+  name: "HTTP Header Checks",
+  type: "response-processor"
+};
+
+var headers = ["X-XSS-Protection", "Access-Control-Allow-Origin"]; // May be an issue when these headers are present
+
+
+// TODO: "X-Frame-Options" -> Burp: Frameable response (potential Clickjacking)
+// TODO: "Content-Type: text/html" missing charset -> Burp: HTML does not specify charset - this should be its own module since it also checks response body
+
+function run(request, response, ctx) {
+	var uri = String(request.requestLine.uri);
+	var uripart = uri.replace(/\?.*/, "");
+	
+	for (var i = 0; i < headers.length; i++) {
+		if (response.hasHeader(headers[i])) {
+			var hdr = response.getFirstHeader(headers[i]);
+			if (hdr.name == "X-XSS-Protection" && hdr.value == "0") {
+				ctx.addStringHighlight(hdr);
+				ctx.alert("vinfo-xss-filter-disabled", request, response, {
+          output: hdr,
+					resource: request.requestLine.uri,
+          key: "vinfo-xss-filter-disabled" + uripart
+        });
+			}
+			
+			if (hdr.name == "Access-Control-Allow-Origin" && hdr.value == "*") {
+				ctx.addStringHighlight(hdr);
+				ctx.alert("vinfo-insecure-cors-ac", request, response, {
+					output: hdr,
+					resource: request.requestLine.uri,
+					key: "vinfo-insecure-cors-ac" + uripart
+				});
+			}
+		}
+	}
+}
