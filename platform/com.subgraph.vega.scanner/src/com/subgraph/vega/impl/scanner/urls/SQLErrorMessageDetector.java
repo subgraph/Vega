55df08bc7e7165466fc5cc02ff6e94b5acea019a
dma <dma@subgraph.com>
2013-02-23 15:14:11 -0500

    Various DB error strings

--- platform/com.subgraph.vega.scanner/src/com/subgraph/vega/impl/scanner/urls/SQLErrorMessageDetector.java
+++ platform/com.subgraph.vega.scanner/src/com/subgraph/vega/impl/scanner/urls/SQLErrorMessageDetector.java
@@ -11,8 +11,41 @@ import com.subgraph.vega.api.scanner.IInjectionModuleContext;
 public class SQLErrorMessageDetector {
 	private final static String SQL_INJECTION_ALERT = "vinfo-sql-inject";
 	private final static List<String> ERROR_STRINGS = Arrays.asList(
-			"<b>Warning</b>:  MySQL: ",
-			"Unclosed quotation mark"
+			
+			"Incorrect syntax near",                             // MS-SQL Server
+			"Unclosed quotation mark",					         // MS-SQL Server
+			"Dynamic SQL Error",						         // MS-SQL Server
+			"SqlClient.SqlException: Syntax error",				 // ASP.Net / MS SQL Server
+			"[Microsoft][ODBC SQL Server Driver]",               // MS Generic ODBC Error
+			"Microsoft OLE DB Provider for ODBC Drivers</font>", // MS OLE 
+			"Microsoft OLE DB Provider for ODBC Drivers</FONT>", // MS OLE
+
+			"Syntax error in string in query expression",        // MS Access
+			
+			"<b>Warning</b>:  MySQL: ",                          // MySQL
+			"You have an error in your SQL syntax",              // MySQL
+			"supplied argument is not a valid MySQL",            // MySQL
+			
+			"PostgreSQL query failed:",                          // PostgreSQL
+			"unterminated quoted string at or near",             // PostgreSQL
+			"syntax error at or near",                           // PostgreSQL
+			"invalid input syntax for integer:",		         // PostgreSQL
+			"Query failed: ERROR: syntax error",		         // PostgreSQL
+			
+			"Unexpected end of command in statement",	         // Java
+			"java.sql.SQLException:",					         // Java
+			
+			"quoted string not properly terminated",	         // Oracle
+			"SQL command not properly ended",                    // Oracle
+			"unable to perform query",
+			
+			"[DM_QUERY_E_SYNTAX]",						         // DQL
+			
+			"[Macromedia][SQLServer JDBC Driver]",				 // CF
+			
+			"DB2 SQL Error:"     								 // DB2
+			
+			
 			);
 	
 	private final ResponseAnalyzer responseAnalyzer; 
@@ -35,6 +68,6 @@ public class SQLErrorMessageDetector {
 	
 	private void processDetectedErrorMessage(IInjectionModuleContext ctx, HttpUriRequest request, IHttpResponse response, String errorString) {
 		ctx.addStringHighlight(errorString);
-		responseAnalyzer.alert(ctx, SQL_INJECTION_ALERT, "SQL Error Message Detected", request, response);
+		responseAnalyzer.alert(ctx, SQL_INJECTION_ALERT, "SQL Error Message Detected", request, response, null);
 	}
 }
