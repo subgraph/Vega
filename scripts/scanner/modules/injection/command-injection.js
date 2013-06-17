99fe459fdcd455ba3cdefbf3df718985f47dc538
David McKinney <mckinney@subgraph.com>
2013-06-17 01:54:32 -0400

    Updated Blind OS command injection module to bail out whilst avoiding global "detected" variable.

--- scripts/scanner/modules/injection/command-injection.js
+++ scripts/scanner/modules/injection/command-injection.js
@@ -1,110 +1,85 @@
 var module = {
-  name: "Blind OS Command Injection",
-  category: "Injection Modules",
-  differential: false
+	name: "Blind OS Command Injection",
+	category: "Injection Modules",
+	differential: false
 };
 
-var detected = false;
-var alerted = false;
-var average = 0;
-
 var unixMetaChars = ['; ', '" ; ', "' ; ", "| ", '"| ', "'| "]; 
-var windowsMetaChars = ['&& ', '& ', '|| '];
+var windowsMetaChars = ['', '" '];
 
 var commands = [
-  {command: "/bin/sleep 30 ;", os: "Linux/Unix"},
-  {command: "/usr/bin/sleep 30 ;", os: "Linux/Unix"},
-  {command: "/sbin/sleep 30 ;", os: "Linux/Unix"},
-  {command: "ping.exe -n 30 127.0.0.1 > nul & ::", os: "Windows"},
-  {command: "timeout 30 & ::", os: "Windows"}
+  {command: "/bin/sleep 31 ;", os: "Linux/Unix"},
+  {command: "ping.exe -n 31 127.0.0.1", os: "Windows"},
+
 ];
 
 var alteredRequests = [];
 
-function initialize(ctx) {
-	for (var i = 0; i < commands.length; i++) {
-		for (var j = 0; j < unixMetaChars.length; j++) {
-			if (commands[i].os === "Linux/Unix") { 
-				alteredRequests.push({ 
-					commandString: unixMetaChars[j] + commands[i].command,
-					os: commands[i].os
-				});
-			}
-		}
-		for (var k = 0; k < windowsMetaChars.length; k++) {
-			if (commands[i].os === "Windows") { 
-				alteredRequests.push({ 
-					commandString: windowsMetaChars[k] + commands[i].command,
-					os: commands[i].os
-				});
-			}
+for (var i = 0; i < commands.length; i++) {
+	for (var j = 0; j < unixMetaChars.length; j++) {
+		if (commands[i].os === "Linux/Unix") { 
+			alteredRequests.push({ 
+				commandString: unixMetaChars[j] + commands[i].command,
+				os: commands[i].os
+			});
 		}
 	}
-		
-  var ps = ctx.getPathState();
-  
-  if (ps.isParametric()) {
-    ctx.submitMultipleAlteredRequests(checkTiming, ["vega1", "vega2"], false);    
+	for (var k = 0; k < windowsMetaChars.length; k++) {
+		if (commands[i].os === "Windows") { 
+			alteredRequests.push({ 
+				commandString: windowsMetaChars[k] + commands[i].command,
+				os: commands[i].os
+			});
+		}
+	}
+}
+
+var checkTiming = function(ctx, currentIndex) {
+	if (ctx.getSavedResponse(currentIndex).milliseconds > 30000) {
+		return true;
+	}
+	return false;
+};
+
+function initialize(ctx) {
+	var ps = ctx.getPathState();
+	if (ps.isParametric()) {
+		ctx.submitAlteredRequest(process, alteredRequests[0].commandString, false, 0);
     ps.incrementFuzzCounter();
-  }
-}
+	}
+};
 
-function checkTiming(req, res, ctx) {
-  
-  if (res.fetchFail) {
-    ctx.error(req, res, "During command injection checks");
-    ctx.setModuleFailed();
-    return;
-  }
-  
-  ctx.addRequestResponse(req, res);
-  if (ctx.incrementResponseCount() < 2) return;
-  var first = ctx.getSavedResponse(0).milliseconds;
-  var second = ctx.getSavedResponse(1).milliseconds;
-  average = first + second / 2;
-  var firstRequest = alteredRequests.pop();
-  ctx.submitAlteredRequest(process, firstRequest.commandString, false, 0);
-}
-
-function process(req2, res2, ctx2) {
-  
-	if (res2.fetchFail) {
+function process(req, res, ctx) {
+	
+	if (res.fetchFail) {
     ctx2.error(req, res, "During command injection checks");
     ctx2.setModuleFailed();
     return;
   }
+	
+	var ps = ctx.getPathState();
+	var currentIndex = ctx.getCurrentIndex();
+	ctx.addRequestResponse(req, res);
+  ctx.incrementResponseCount();
+  var detected = checkTiming(ctx, currentIndex);
   
-	var ps = ctx2.getPathState();
-  var currentIndex = ctx2.getCurrentIndex();
-  ctx2.addRequestResponse(req2, res2);
-  ctx2.incrementResponseCount();
+	if (detected) {
+		var uri = String(req.requestLine.uri);
+		var uripart = uri.replace(/\?.*/, "");
+		ctx.alert("vinfo-shell-inject", ctx.getSavedRequest(currentIndex), ctx.getSavedResponse(currentIndex), {
+			output: res.bodyAsString,
+			key: "vinfo-shell-inject:" + uripart + ":" + ps.getFuzzableParameter().name,
+			resource: uripart,
+			detectiontype: alteredRequests[currentIndex].os + " Blind Timing Analysis Checks",
+			param: ps.getFuzzableParameter().name
+		});
+	} else {
+		if (currentIndex + 1 < alteredRequests.length) {
+			ctx.submitAlteredRequest(process, alteredRequests[currentIndex + 1].commandString, false, currentIndex + 1);
+		}
+	};
 
-  if ((average < 30000) && (ctx2.getSavedResponse(currentIndex).milliseconds > 30000)) {
-    detected = true;
-  }
-
-  if (detected && !alerted) {    
-    alerted = true;
-    var uri = String(req2.requestLine.uri);
-    var uripart = uri.replace(/\?.*/, "");
-    ctx2.alert("vinfo-shell-inject", ctx2.getSavedRequest(currentIndex), ctx2.getSavedResponse(currentIndex), {
-      output: res2.bodyAsString,
-      key: "vinfo-shell-inject:" + uripart + ":" + ps.getFuzzableParameter().name,
-      resource: uripart,
-      detectiontype: alteredRequests[alteredRequests.length - 1].os + " Blind Timing Analysis Checks",
-      param: ps.getFuzzableParameter().name
-    }); 
-  } else {
-  	var nextRequest = alteredRequests.pop();
-    if (nextRequest) {
-      ctx2.submitAlteredRequest(process, nextRequest.commandString, false, currentIndex + 1);
-    }
-  }
-
-  if (ctx2.allResponsesReceived()) {
-    ps.decrementFuzzCounter();
-  }
-
-}
-
-
+	if (ctx.allResponsesReceived()) {
+		ps.decrementFuzzCounter();
+	}
+};
